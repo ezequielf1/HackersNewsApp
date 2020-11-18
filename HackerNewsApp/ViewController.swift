af24e9ded9a601ac41bedc6b8a1713397647b2ca
brian.fritz <brian.fritz@etermax.com>
2020-11-18 14:42:01 -0300

    Init swift project

---
+++ HackerNewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HackerNewsApp
+//
+//  Created by Brian Ezequiel Fritz on 18/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
